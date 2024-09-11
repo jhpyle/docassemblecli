b2f7192e3bfc87f83042f02d8576ee33d4679e58
Jonathan Pyle <jhpyle@gmail.com>
2024-09-11 12:49:16 -0400

    Bump version: 0.0.18 → 0.0.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="docassemblecli",
-    version="0.0.18",
+    version="0.0.19",
     author="Jonathan Pyle",
     author_email="jhpyle@gmail.com",
     description="CLI utilities for using docassemble",
